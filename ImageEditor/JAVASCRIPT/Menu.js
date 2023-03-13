51785ecfdcc82ce12d82d9032cdda965d2e3a14d
mmustafoff <80255941+mmustafoff@users.noreply.github.com>
2023-03-13 16:54:16 +0200

    Add files via upload

---
+++ ImageEditor/JAVASCRIPT/Menu.js
@@ -0,0 +1,40 @@
+const M1 = document.getElementById('Menu1'); //Menu1(Edits)
+const M2 = document.getElementById('Menu2'); //Menu1(Filters)
+const FilterBtn = document.getElementById('Filters');
+const EditBtn = document.getElementById('Edit');
+const Edit = document.getElementById('ForEdits');
+const Filters = document.getElementById('ForFilters');
+
+const Select = (Clicked) =>{
+    Clicked.style.borderStyle = "outset";
+    Clicked.style.borderWidth = "1px";
+    Clicked.style["boxShadow"] = "0 0 5px #dddddd";
+}
+
+const SwitchToFilters = (From) =>{
+From.style.borderStyle = "none";
+From.style["boxShadow"] = "";
+M1.style.left = "-100%";
+M2.style.left = "100%";
+Select(Filters);
+}
+
+const SwitchToEdit = (From) =>{
+    From.style.borderStyle = "none";
+    From.style["boxShadow"] = "";
+    M1.style.left = "0";
+    M2.style.left = "100%";
+    Select(Edit);
+    }
+
+FilterBtn.addEventListener('click', ()=>{
+    SwitchToFilters(Edit);
+})
+
+EditBtn.addEventListener('click', ()=>{
+    SwitchToEdit(Filters);
+})
+
+window.addEventListener('load', () =>{
+    Select(Edit);
+})
